80341e86a7e7018f7c7aaa1b923f7edaf397573a
Manoj Jahgirdar <52746337+manojjahgirdar@users.noreply.github.com>
2025-04-23 16:14:29 +0530

    Create langgraph_agent_remote_mcp.py

---
+++ src/langgraph_agent_remote_mcp.py
@@ -0,0 +1,58 @@
+from ibm_watsonx_ai.metanames import GenTextParamsMetaNames
+from langchain_ibm import ChatWatsonx
+from langchain_openai import ChatOpenAI
+
+from contextlib import asynccontextmanager
+from langchain_mcp_adapters.client import MultiServerMCPClient
+
+from langgraph.prebuilt import create_react_agent
+
+import asyncio
+import os
+
+if os.getenv('OPENAI_API_KEY'):
+    llm = ChatOpenAI(model="o3-mini")
+elif os.getenv('WATSONX_APIKEY'):
+    llm = ChatWatsonx(
+        model_id=os.environ['MODEL_ID'],
+        url=os.environ['WATSONX_URL'],
+        project_id=os.environ['WX_PROJECT_ID'],
+        params={
+            GenTextParamsMetaNames.MAX_NEW_TOKENS: 500,
+            GenTextParamsMetaNames.MIN_NEW_TOKENS: 20,
+            GenTextParamsMetaNames.DECODING_METHOD: 'greedy',
+            
+        },
+    )
+else:
+    print('Export OPENAI_API_KEY or WATSONX_APIKEY to initialize OpenAI or Watsonx LLM.')
+    exit(1)
+
+@asynccontextmanager
+async def main():
+    async with MultiServerMCPClient(
+        {
+            "mcp_server": {
+                "url": "https://manojs-mcp-server.xyz.codeengine.appdomain.cloud/sse", # Replace with your Remote MCP Server URL
+                "transport": "sse"
+            }
+        }
+    ) as client:
+        agent = create_react_agent(
+                llm,
+                tools=client.get_tools(),
+                prompt="You are a helpfull assistant."
+            )
+        yield agent
+
+async def invoke_agent(query):
+    async with main() as agent:
+        agent_response = await agent.ainvoke({"messages": query})
+        print("==== Final Answer ====")
+        print(agent_response['messages'][-1].content)
+
+
+if __name__ == "__main__":
+    
+    query = "Give me 5 tourist attractions and weather details for Bengaluru"
+    asyncio.run(invoke_agent(query=query))
