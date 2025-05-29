2b51abba71f5ed99b618978cc7a946a3b199e4ce
manojjahgirdar <jahgirdarmanoj@gmail.com>
2025-05-29 12:51:50 +0530

    updated search tool with Tavily

--- tools/search_tool.py
+++ tools/search_tool.py
@@ -1,22 +1,22 @@
 from mcp_server import mcp
-from langchain_community.tools import DuckDuckGoSearchRun
-import os
+from tavily import TavilyClient
 
 @mcp.tool()
 def search_tool(query: str) -> str:
     """
-    Search the web for relevant information using DuckDuckGo Search.
+    Search the web for relevant information using Tavily Search.
 
     Args:
         query (str): The search query string.
 
     Returns:
-        dict: A dictionary of search results obtained from DuckDuckGo search.
+        dict: A dictionary of search results obtained from Tavily search.
     """
     try:
-        search = DuckDuckGoSearchRun()
-        response = search.invoke(query)
+        # NOTE: You need to set the TAVILY_API_KEY environment variable to use this tool.
+        tavily_client = TavilyClient()
+        response = tavily_client.search(query=query, max_results=5, include_answer=True)
         return response
     except Exception as e:
         # Log the exception or handle it as needed
-        return f"An error occurred while while invoking the tool DuckDuckGo search tool. Here is the logs, try to analyze it and retry invoking the tool possibly with different payload. Logs: {str(e)}"
+        return f"An error occurred while while invoking the tool Tavily search tool. Here is the logs, try to analyze it and retry invoking the tool possibly with different payload. Logs: {str(e)}"
